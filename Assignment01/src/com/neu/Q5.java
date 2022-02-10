7e59945f0bf4416ab5632be6e4357ef239b4d0bf
Hannah397-1 <75960457+Hannah397-1@users.noreply.github.com>
2022-02-09 17:31:46 -0800

    Update Q5.java

--- Assignment01/src/com/neu/Q5.java
+++ Assignment01/src/com/neu/Q5.java
@@ -2,7 +2,7 @@ package com.neu;
 
 public class Q5 {
     static boolean isLeapYear(int year){
-        if(year%4 == 0) return true;
+        if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) return true;
         return false;
     }
 
